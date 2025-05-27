a9543a9c1e818936c46df4823fa01f34031ff0a3
Bauti123456789 <167232239+Bauti123456789@users.noreply.github.com>
2025-05-27 11:57:07 -0300

    r3124t5yhty5432

---
+++ Assets/scripts/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int scorePoints;
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision Collision)
+    {
+        if (Collision.gameObject.name=="player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
